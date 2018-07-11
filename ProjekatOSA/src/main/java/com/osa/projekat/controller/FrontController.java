08168e2188dce72709fe53be6cb035a7dd9571bc
Marko Krizan <markokrizan64@gmail.com>
2018-07-11 08:38:59 +0200

    boze me sacuvaj i sakloni

--- ProjekatOSA/src/main/java/com/osa/projekat/controller/FrontController.java
+++ ProjekatOSA/src/main/java/com/osa/projekat/controller/FrontController.java
@@ -1,13 +1,12 @@
 package com.osa.projekat.controller;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Controller;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FrontController {
+	
 	
 	
 	
@@ -17,20 +16,15 @@ public class FrontController {
         return "index";
 	}
 	
-	/*
-	@RequestMapping("/test")
-	public String test() {
-        return "testPage.html";
-	}
-	*/
 	
-	
-	
+
+
 	@RequestMapping("/test/{id}")
 	public String test(@PathVariable("id") String id) {
 		//ovde je provera ustvari da li ga ima pa ako ga nema onda error 
         return "testPage";
 	}
+	
 	
 	
 }
